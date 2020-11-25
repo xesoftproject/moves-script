b508c6b2df709eb73cabda7c056cd4a7c65db498
Vito De Tullio <vito.detullio@gmail.com>
2020-11-25 17:53:47 +0100

    local path

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 
 # should be called for update the sources; also calls stop and start
 
-. stop.sh
+. "$(dirname "${0}")/stop.sh"
 
 . venv/bin/activate
 git pull
 pip install --upgrade ./moves
 deactivate
 
-. start.sh
+. "$(dirname "${0}")/start.sh"
