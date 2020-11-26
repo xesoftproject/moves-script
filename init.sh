03046531bb8afe05dcb5544dddc43521af039451
Vito De Tullio <vito.detullio@gmail.com>
2020-11-26 09:19:15 +0100

    the public hostme now is fixed :)

--- init.sh
+++ init.sh
@@ -14,7 +14,6 @@ git clone https://github.com/xesoftproject/moves.git
 # update sources
 PUBLIC_HOSTNAME='ec2-'"$(curl -s ifconfig.me|sed 's/\./-/g')"'.eu-west-1.compute.amazonaws.com'
 sed -i "/EC2_HOSTNAME/s/'[^']*'/'${HOSTNAME}'/" moves/moves/configurations/__init__.py
-sed -i "/REST_HOSTNAME/s/'[^']*'/'${PUBLIC_HOSTNAME}'/" moves/moves/configurations/__init__.py
 pushd moves; git add .; git commit -m 'update aws stuff'; popd
  # TODO push
 
