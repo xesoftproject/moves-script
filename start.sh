f03d53fd71f14da5146964805d11478f5a527e3f
dario.brambilla.fincons <dario.brambilla.fincons@skytv.it>
2021-01-10 18:21:43 +0100

    update path node

--- start.sh
+++ start.sh
@@ -16,6 +16,6 @@ echo $! > MOVES_CHAT_PID
 deactivate
 
 #CHAT
-CONFIG_URL=s3:xesoft/config.yml sudo node _root/app/app.js &
+CONFIG_URL=s3:xesoft/config.yml sudo node chat/_root/app/app.js &
 echo $! > CHAT_PID
 
