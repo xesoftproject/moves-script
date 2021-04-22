7b64d9764bd554f2935dddab462458dd11369589
Vito De Tullio <vito.detullio@gmail.com>
2021-04-22 10:14:13 +0200

    node spawn a tree

--- stop.sh
+++ stop.sh
@@ -3,4 +3,4 @@
 # should be called to stop the applications
 
 sudo kill "$(cat MOVES_REST_PID)" && rm MOVES_REST_PID
-sudo kill "$(cat CHAT_PID)" && rm CHAT_PID
+sudo kill -- $(pstree -pTU | grep $(cat CHAT_PID) | sed -e 's/^[^(]*(//g' -e 's/)[^(]*(/ /g' -e 's/).*$//g') && rm CHAT_PID
